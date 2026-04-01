593c2303c106f30232742db812b0ffcd79b8a6e9
Pal <pal@gmail.com>
2026-04-01 11:51:32 +0530

    Update database migration and configure decimal precision for transaction amount

---
+++ ExpenseTrackerApi/Models/Transaction.cs
@@ -0,0 +1,18 @@
+﻿namespace ExpenseTrackerApi.Models
+{
+    public class Transaction
+    {
+        public int TransactionId { get; set; }        
+        public decimal Amount { get; set; }
+        public required string Type { get; set; }
+        public DateTime Date { get; set; }
+
+        //Foregin Key
+        public int UserId { get; set; }
+        public User ? User { get; set; }
+
+        //Foreign Key
+        public int CategoryId { get; set; }
+        public Category ? Category { get; set; }
+    }
+}
---
+++ ExpenseTrackerApi/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace ExpenseTrackerApi.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public required string Email { get; set; }
+        public required string PasswordHash { get; set; }
+    }
+}
